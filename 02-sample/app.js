69888938baf97250aac704d871d9e7c45bc0362b
Mosframe <ceo.mosframe@gmail.com>
2017-04-30 17:21:36 +0900

    서버에서 탄환을 무작위로 생성하여 동기화한다.

--- 02-sample/app.js
+++ 02-sample/app.js
@@ -48,7 +48,7 @@ var Entity = function() {
 }
 
 // -----------------------------------------------------------------------------
-// 플레이어 클래스
+// 플레이어
 // -----------------------------------------------------------------------------
 var Player = function(id) {
     var self            = Entity();
@@ -125,6 +125,53 @@ Player.updates = function(){
 }
 
 // -----------------------------------------------------------------------------
+// 탄환
+// -----------------------------------------------------------------------------
+var Bullet = function(angle){
+    var self = Entity();
+    self.id = Math.random();
+    self.moveSpeedX = Math.cos(angle/180*Math.PI) * 10;
+    self.moveSpeedY = Math.sin(angle/180*Math.PI) * 10;
+
+    self.lifeTime = 100;
+    self.toRemove = false;
+
+    var supper_update = self.update;
+    self.update = function() {
+        if( self.lifeTime-- <= 0) {
+            self.toRemove = true;
+        }
+        if( self.ToRemove ) {
+            delete Bullet.list[self.id];
+        }
+        supper_update();
+    }
+    Bullet.list[self.id] = self;
+    return self;
+}
+Bullet.list = {};
+Bullet.updates = function() {
+
+    // 무작위로 탄환 생성
+    if( Math.random() < 0.1 ) {
+        Bullet( Math.random()*360 );
+    }
+
+    var pack = [];
+    // 모든 플레이어들의 각자 변경된 위치정보를 갱신하고 패키지에 담는다.
+    for( var bulletId in Bullet.list ) {
+        var bullet = Bullet.list[bulletId];
+        bullet.update();
+        pack.push({
+            x:bullet.x,
+            y:bullet.y,
+        });
+    }
+    return pack;
+}
+
+
+// -----------------------------------------------------------------------------
 
 // Socket.io를 통해 WebSocket 서비스를 시작한다.
 var io = require('socket.io')(server,{});
@@ -145,7 +192,13 @@ io.sockets.on('connection', function(socket){
 
 // 서버 틱 업데이트 : 25fps
 setInterval( function(){
-    var pack = Player.updates();
+
+    // 모든 엔티티들 갱신
+    var pack = {
+        player:Player.updates(),
+        bullet:Bullet.updates(),
+    }
+
     // 모든 클라이언트들에게 패키지 데이터를 보낸다.
     for( var socketId in sockets ) {
         var socket = sockets[socketId];
